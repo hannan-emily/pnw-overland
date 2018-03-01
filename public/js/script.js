8426f31ed9f62f2d375e888834cce9ca55a9cf10
hannan-emily <emilyhannan@gmail.com>
2018-02-28 16:56:46 -0800

    added new video links to marker capability. on marker click event, a infowindow pops up and a video for that portion of the trail autoplays

--- public/js/script.js
+++ public/js/script.js
@@ -1,153 +1,158 @@
-// //RENDERING THE TRAIL MAP
-// function initMap() {
-//         //this sets the location variable from two coordinates
-//         var place = {lat: -25.363, lng: 131.044};
-//
-//         //this is setting the map to THIS new location, using the google maps method
-//         //this calls the place variable (now as uluru)
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 4,
-//           center: place
-//         });
-//
-//         //SETTING INFO INTO THE MARKER INFOWINDOW (DISPLAYS ON CLICK EVENT)
-//         //insert db.data.whatever into here
-//         var contentString = '<div id="content">'+
-//             '<div id="siteNotice">'+
-//             '</div>'+
-//             '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-//             '<div id="bodyContent">'+
-//             '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-//             'sandstone rock formation in the southern part of the '+
-//             'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-//             'south west of the nearest large town, Alice Springs; 450&#160;km '+
-//             '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-//             'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-//             'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-//             'Aboriginal people of the area. It has many springs, waterholes, '+
-//             'rock caves and ancient paintings. Uluru is listed as a World '+
-//             'Heritage Site.</p>'+
-//             '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-//             'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-//             '(last visited June 22, 2009).</p>'+
-//             '</div>'+
-//             '</div>';
-//
-//         //SETTING THE CONTENT INTO THE MARKER INFOWINDO
-//         var infowindow = new google.maps.InfoWindow({
-//           content: contentString
-//         });
-//
-//         //MARKER IS TIED TO THE PLACE VARIABLE & THOSE COORDINATES (FOR NOW)
-//         var marker = new google.maps.Marker({
-//           position: place,
-//           map: map,
-//           title: 'Uluru (Ayers Rock)'
-//         });
-//
-//         //ON CLICK LISTENER FOR MARRKER TO CALL THE INFOWINDOW & CONTENT
-//         marker.addListener('click', function() {
-//           infowindow.open(map, marker);
-//         });
-//       }
 
-
-// //THIS MAP DISPLAYS A ROUTE, BUT DOES NOT DISPLAY MARKERS
-// function initMap() {
-//   var directionsService = new google.maps.DirectionsService();
-//   var directionsDisplay = new google.maps.DirectionsRenderer();
-//   var haight = new google.maps.LatLng(37.7699298, -122.4469157);
-//   var oceanBeach = new google.maps.LatLng(37.7683909618184, -122.51089453697205);
-//   var mapOptions = {
-//     zoom: 14,
-//     center: haight
-//   }
-//   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//   directionsDisplay.setMap(map);
-// }
-//
-// function calcRoute() {
-//   // var selectedMode = document.getElementById('mode').value;
-//   var request = {
-//       origin: {lat: 37.77, lng: -122.447},
-//       destination: {lat: 37.768, lng: -122.511},
-//       // Note that Javascript allows us to access the constant
-//       // using square brackets and a string value as its
-//       // "property."
-//       travelMode: 'DRIVING'
-//   };
-//   directionsService.route(request, function(response, status) {
-//     if (status == 'OK') {
-//       directionsDisplay.setDirections(response);
-//     }
-//   });
-// }
-
-//THIS MAP SHOULD DISPLAY POINTS & THE ROUTES, BUT ONLY DISPLAYS A SINGLE MAP
-// function initMap() {
-//         var directionsDisplay = new google.maps.DirectionsRenderer;
-//         var directionsService = new google.maps.DirectionsService;
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 14,
-//           center: {lat: 37.77, lng: -122.447}
-//         });
-//         directionsDisplay.setMap(map);
-//
-//         calculateAndDisplayRoute(directionsService, directionsDisplay);
-//       }
-//
-//       function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-//
-//         directionsService.route({
-//           origin: {lat: 37.77, lng: -122.447},  // Haight.
-//           destination: {lat: 37.768, lng: -122.511},  // Ocean Beach.
-//           // Note that Javascript allows us to access the constant
-//           // using square brackets and a string value as its
-//           // "property."
-//           travelMode: 'DRIVING'
-//         }, function(response, status) {
-//           if (status == 'OK') {
-//             directionsDisplay.setDirections(response);
-//           } else {
-//             window.alert('Directions request failed due to ' + status);
-//           }
-//         });
-//       }
-
+////////////////////////////2 POINT DIRECTION TRAIL MAP WITH HARDCODED MARKERS///////////////
 var directionsService = null;
 var directionsDisplay = null;
+var start = null;
+var end = null;
+
 
 function initMap() {
   console.log("in the initMap function");
   directionsService = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer();
-  var haight = new google.maps.LatLng(37.7699298, -122.4469157);
-  var oceanBeach = new google.maps.LatLng(37.7683909618184, -122.51089453697205);
-  var mapOptions = {
-    zoom: 14,
-    center: haight
-  }
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  start = new google.maps.LatLng(49.613955, -121.820801); //harrison hot springs
+  end = new google.maps.LatLng(49.863426, -121.442574); //boston bar
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: start,
+      mapTypeId: 'satellite'
+    });
+
   directionsDisplay.setMap(map);
   calcRoute();
-}
 
-function calcRoute() {
-  var selectedMode = "DRIVING";
-  var request = {
-    origin: {lat: 37.77, lng: -122.447},  // Haight.
-    destination: {lat: 37.768, lng: -122.511},
-      // Note that Javascript allows us to access the constant
-      // using square brackets and a string value as its
-      // "property."
+  //MARKERS IN HERE ///////
+
+
+  var contentShovelCreek5km = '<div id="content">'+
+      '<h5 id="firstHeading" class="firstHeading">Shovel Creek 5km ALERT</h5>'+
+      '<div id="bodyContent">'+
+      '<p>' +
+      'WARNING: Big hole in the ground at 5.7km past Shovel Creek Head.' +
+      '</p>'+
+      '<a href="http://ihikebc.com/trips/2011/trip050ShovelCreekFSR.htm">2011 Guide</a>' +
+      '</div>';
+
+  var markerShovelCreek5km = new google.maps.Marker({
+    position: {lat: 49.805572, lng: -121.792502},
+    map: map,
+    title: 'Shovel Creek 5km ALERT'
+  });
+
+  var infoShovelCreek5km = new google.maps.InfoWindow({
+    content: contentShovelCreek5km
+  });
+
+  var contentShovelCreek = '<div id="content">'+
+      '<h5 id="firstHeading" class="firstHeading">Shovel Creek FSR Start Point</h5>'+
+      '<div id="bodyContent">'+
+      '<p>' +
+      'Zero your odometer at the sign, right as the Harrison East FSR merges to Shovel Creek FSR.' +
+      'Travel on Shovel Creek FSR to 11.2 mark, where the road became too narrow, and there was threat of scratching our ride with large stones.' +
+      '</p>'+
+      '<a href="http://ihikebc.com/trips/2011/trip050ShovelCreekFSR.htm">2011 Guide</a>' +
+      '</div>';
+
+  var markerShovelCreek = new google.maps.Marker({
+    position: {lat: 49.782760, lng: -121.862497},
+    map: map,
+    title: 'Shovel Creek'
+  });
+
+  var infoShovelCreek = new google.maps.InfoWindow({
+    content: contentShovelCreek
+  });
+
+/// MARKERS IN HERE //////////
+
+  function calcRoute() {
+    var selectedMode = "DRIVING";
+    var request = {
+      origin: start, // Haight.
+      destination: end,
       travelMode: google.maps.TravelMode[selectedMode]
-  };
-  directionsService.route(request, function(response, status) {
-    console.log("in the directionService.route function");
-    console.log(status);
-    console.log(response);
-    if (status == 'OK') {
-      directionsDisplay.setDirections(response);
-    }
+    };
+
+    preserveViewport: true;
+
+    directionsService.route(request, function(response, status) {
+      console.log("in the directionService.route function");
+      console.log(status);
+      console.log(response);
+      if (status == 'OK') {
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
+  markerShovelCreek.addListener('click', function() {
+    infoShovelCreek.open(map, markerShovelCreek);
+  });
+
+  markerShovelCreek5km.addListener('click', function() {
+    document.getElementById('ytplayer').src = "https://www.youtube.com/embed/rN49Sxg23jQ?start=245&autoplay=1";
+    infoShovelCreek5km.open(map, markerShovelCreek5km);
   });
 }
+// ////////////////////////////2 POINT DIRECTION MAP ///////////////
+
+// /////// MAP WITH WAYPOINTS ///////////////////
+// var directionsService = null;
+// var directionsDisplay = null;
+// var start = null;
+// var end = null;
+//
+// function initMap() {
+//         var directionsService = new google.maps.DirectionsService;
+//         var directionsDisplay = new google.maps.DirectionsRenderer;
+//         start = new google.maps.LatLng(41.878114, -87.629798); //chicago
+//         end = new google.maps.LatLng(44.977753 , -93.265011); //minnneapolis
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 6,
+//           center: {lat: 41.878114, lng: -87.629798}
+//         });
+//         directionsDisplay.setMap(map);
+//
+//         directionsDisplay.setMap(map);
+//         calcRoute();
+//
+//
+//
+//       function calcRoute(directionsService, directionsDisplay) {
+//         var waypts = ['Rochester, MN'];
+//         // var checkboxArray = document.getElementById('waypoints');
+//         // for (var i = 0; i < checkboxArray.length; i++) {
+//         //   if (checkboxArray.options[i].selected) {
+//         //     waypts.push({
+//         //       location: checkboxArray[i].value,
+//         //       stopover: true
+//         //     });
+//         //   }
+//         // }
+//
+//         directionsService.route({
+//           origin: start,
+//           destination: end,
+//           waypoints: waypts,
+//           optimizeWaypoints: true,
+//           travelMode: 'DRIVING'
+//         }, function(response, status) {
+//           if (status === 'OK') {
+//             directionsDisplay.setDirections(response);
+//             var route = response.routes[0];
+//             var summaryPanel = document.getElementById('directions-panel');
+//             summaryPanel.innerHTML = '';
+//             // For each route, display summary information.
+//             for (var i = 0; i < route.legs.length; i++) {
+//               var routeSegment = i + 1;
+//               summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+//                   '</b><br>';
+//               summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+//               summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+//               summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+//             }
+//           } else {
+//             window.alert('Directions request failed due to ' + status);
+//           }
+//         });
+//       }
